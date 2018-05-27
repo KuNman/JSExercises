4a335481c13da6255b3e31033da4f1e1f1259153
KuNman <knejman56@gmail.com>
2018-05-27 12:56:35 +0200

    XHR

---
+++ XHR.js
@@ -0,0 +1,18 @@
+var btn = document.querySelector("#btn");
+var span = document.querySelector("#price");
+
+btn.addEventListener("click", function() {
+
+  var XHR = new XMLHttpRequest();
+
+  XHR.onreadystatechange = function(){
+    if(XHR.readyState == 4 && XHR.status == 200) {
+      var rate = JSON.parse(XHR.responseText).bpi.USD.rate_float;
+      price.innerHTML = rate;
+    }
+  }
+
+  XHR.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json");
+  XHR.send();
+
+})
